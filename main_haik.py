446c10c3e233f899b54f54f9a9be2d88b8f88f02
RGuseynov <roustam@live.fr>
2019-05-24 15:09:28 +0200

    creation classe Map

--- main_haik.py
+++ main_haik.py
@@ -1,85 +1,29 @@
 import folium
 
 
-m = folium.Map(location=[45.750000, 4.850000])
+class Map:
+    def __init__(self, longitude, latitude):
+        self.longitude = longitude
+        self.latitude = latitude
+        self.map = folium.Map(
+            location=[longitude, latitude],
+            tiles='openstreetmap',
+            zoom_start=13,
+            attr='My Data Attribution'
+        )
+
+    def draw_map(self, iter_longitude, iter_latitude):
+        for i in range(len(iter_longitude)):
+            folium.Marker(
+                location=[iter_longitude[i], iter_latitude[i]],
+                icon=folium.Icon(color='white', icon='info-sign')
+            ).add_to(self.map)
+        self.map.save('index.html')
 
 
+list_longitude = [45.750000, 45.7529002, 45.7603831, 45.774195, 45.762,45.7614853, 45.7460481, 45.7348248, 45.7739471]
+list_latitude = [4.8300276, 4.8268543, 4.849664, 4.827882, 4.827, 4.843362, 4.8417503, 4.8741702, 4.8069094]
 
+map1 = Map(list_longitude[0], list_latitude[0])
 
-# type de carte
-folium.Map(
-    location=[45.750000, 4.850000],
-    tiles='openstreetmap',
-    zoom_start=13,
-    attr='My Data Attribution'
-)
-
-tooltip = 'Click me!' #nom de la ville
-
-
-# les markeurs pour les lieux selectionnés
-# folium.Marker([45.750000, 4.850000], popup='<i>Mt. Hood Meadows</i>', tooltip=tooltip).add_to(m)
-
-
-
-folium.Marker(
-    location=[45.7694158, 4.8300276],
-    popup='Mairie 1e',
-    icon=folium.Icon(color='white', icon='info-sign')
-).add_to(m)
-
-folium.Marker(
-    location=[45.7529002, 4.8268543],
-    popup='Mairie 2e',
-    icon=folium.Icon(color='blue', icon='info-sign')
-).add_to(m)
-
-folium.Marker(
-    location=[45.7603831, 4.849664],
-    popup='Mairie 3e',
-    icon=folium.Icon(color='blue', icon='info-sign')
-).add_to(m)
-
-folium.Marker(
-    location=[45.774195, 4.827882],
-    popup='Mairie 4e',
-    icon=folium.Icon(color='orange', icon='info-sign')
-).add_to(m)
-
-folium.Marker(
-    location=[45.762, 4.827],
-    popup='Mairie 5e',
-    tooltip=tooltip,
-    icon=folium.Icon(color='pink', icon='info-sign')
-).add_to(m)
-
-folium.Marker(
-    location=[45.7614853, 4.843362],
-    popup='Mairie 6e',
-    icon=folium.Icon(color='purple', icon='info-sign')
-).add_to(m)
-
-folium.Marker(
-    location=[45.7460481, 4.8417503],
-    popup='Mairie 7e',
-    icon=folium.Icon(color='red', icon='info-sign')
-).add_to(m)
-
-folium.Marker(
-    location=[45.7348248, 4.8741702],
-    popup='Mairie 8e',
-    icon=folium.Icon(color='green', icon='info-sign')
-).add_to(m)
-
-folium.Marker(
-    location=[45.7739471, 4.8069094],
-    popup='Mairie 9e',
-    icon=folium.Icon(color='lightblue', icon='info-sign')
-).add_to(m)
-
-
-# lat et long lorsque l'util clique sur la carte
-# m.add_child(folium.LatLngPopup())
-
-
-m.save('index.html')
+map1.draw_map(list_longitude, list_latitude)
