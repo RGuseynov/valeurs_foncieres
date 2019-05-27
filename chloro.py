9ccf198ae96c0a848fd286a109f0e03f6fea88f9
Haikouhi <h.oroudjian@protonmail.com>
2019-05-27 14:19:31 +0200

    testing chloro

--- chloro.py
+++ chloro.py
@@ -4,20 +4,17 @@ import pandas as pd
 import csv
 
 # how-to here (https://python-graph-gallery.com/292-choropleth-map-with-folium/)
-geo_arrond = os.path.join('lyon_arron.json')
+#geo_arrond = os.path.join('lyon_arron.json')
+geo_arron = "https://transcode.geo.data.gouv.fr/services/5bfd5598cb932f8781e55ede/feature-types/ms:adr_voie_lieu.adrarrond?format=GeoJSON&projection=WGS84"
 
-
-donnees = os.path.join('../full.csv')
-donnees = pd.read_csv('../full.csv')
 
 # Initialize the map:
 m = folium.Map(location=[45.750000, 4.850000], zoom_start=13)
  
 # Add the color for the chloropleth:
 m.choropleth(
- geo_data=geo_arrond,
+ geo_data=geo_arron,
  name='choropleth',
- data=donnees,
  columns=['latitude', 'longitude'],
  key_on='feature.id',
  fill_color='YlGn',
@@ -27,7 +24,13 @@ m.choropleth(
 )
 folium.LayerControl().add_to(m)
  
+ 
+# folium.GeoJson(
+#     urlArron,
+#     name='geojson'
+# ).add_to(m)
+
 # Save to html
 m.save('test.html')
 
- 
+
